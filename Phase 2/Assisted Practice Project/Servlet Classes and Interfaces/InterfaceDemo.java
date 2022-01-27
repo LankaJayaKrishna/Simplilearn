b88fd0fb0940e47f7061e4941612516b0c427e14
LankaJayaKrishna <58719669+LankaJayaKrishna@users.noreply.github.com>
2022-01-27 18:46:13 +0530

    Add files via upload

--- Phase 2/Assisted Practice Project/Servlet Classes and Interfaces/InterfaceDemo.java
+++ Phase 2/Assisted Practice Project/Servlet Classes and Interfaces/InterfaceDemo.java
@@ -1,36 +1,35 @@
-
-
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
 
 /**
- * Servlet implementation class InterfaceDemo
- */
-public class InterfaceDemo extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+* Servlet implementation class InterfaceDemo
+*/
+@WebServlet("/InterfaceDemo")
+public class InterfaceDemo implements Servlet {
+        
+        ServletConfig config=null;
+          public void init(ServletConfig config){
+              this.config=config;
+              System.out.println("Initialization complete");
+           }
 
-    /**
-     * Default constructor. 
-     */
-    public InterfaceDemo() {
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
-
+           public void service(ServletRequest req,ServletResponse res)
+           throws IOException,ServletException{
+               res.setContentType("text/html");
+               PrintWriter pwriter=res.getWriter();
+               pwriter.print("<html>");
+               pwriter.print("<body>");
+               pwriter.print("In the service() method<br>");
+               pwriter.print("</body>");
+               pwriter.print("</html>");
+           }
+           public void destroy(){
+               System.out.println("In destroy() method");
+           }
+           public ServletConfig getServletConfig(){
+               return config;
+           }
+           public String getServletInfo(){
+               return "This is a sample servlet info";
+           }
 }
